a23a8f00c5422b1231a089ed4c4e7ee72e36f2fb
lichtenegger <johanna.lichtenegger@gmail.com>
2015-11-21 21:49:18 +0100

    added testuser

---
+++ TestDaten/B01_TestUser.sql
@@ -0,0 +1,36 @@
+use dbBricoMarche;
+GO
+
+-- Test: Land
+
+INSERT INTO Land(Bezeichnung) VALUES('Testanien');
+GO
+
+-- Test: Ort
+
+INSERT INTO Ort(PLZ, Bezeichnung, Land_ID) VALUES('00000', 'Testington',(
+	SELECT ID FROM LAND
+		WHERE Bezeichnung = 'Testanien'
+));
+GO
+
+-- Test: Gruppe
+
+INSERT INTO Gruppe(Bezeichnung) VALUES('Tester');
+GO
+
+-- Test: Benutzer
+
+INSERT INTO Benutzer(Benutzername, Vorname, Nachname, Geburtsdatum, 
+					Passwort, Aktiv, Adresse, Ort_ID, Gruppe_ID)
+VALUES('test@mail.me', 'Testana', 'Testich', '01.02.1921',
+		HASHBYTES('SHA2_256', 'test'), 1, 'Testweg 12', 
+		(SELECT ID FROM ORT WHERE PLZ = '00000'),
+		(SELECT ID FROM Gruppe WHERE Bezeichnung = 'Tester')
+);
+GO
+
+-- Ende
+
+Use master;
+GO
