99f00967f142f1ff1fb8d0ee4105703e0fa0d8cf
lichtenegger <johanna.lichtenegger@gmail.com>
2015-11-25 12:07:54 +0100

    WIP Gebrauchsdaten

---
+++ GebrauchsDaten/Gruppe-Daten.sql
@@ -0,0 +1,7 @@
+use dbBricoMarche;
+
+INSERT INTO Gruppe VALUES('Benutzer');
+INSERT InTO Gruppe VALUES('Administrator');
+GO
+
+use master;
---
+++ GebrauchsDaten/Land-Ort-Daten.sql
@@ -0,0 +1,5 @@
+use dbBricoMarche;
+
+INSERT INTO Land VALUES('Deutschland');
+INSERT INTO Land VALUES('Österreich');
+INSERT INTO Land VALUES('Schweiz');
