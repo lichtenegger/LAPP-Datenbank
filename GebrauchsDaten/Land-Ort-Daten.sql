625bb4714f5efd6ce361815e68b824f01cd26b72
lichtenegger <johanna.lichtenegger@gmail.com>
2015-11-25 14:51:17 +0100

    WIP Data-Import plz

--- GebrauchsDaten/Land-Ort-Daten.sql
+++ GebrauchsDaten/Land-Ort-Daten.sql
@@ -1,5 +1,23 @@
+-- ORTE ÖSTERREICH IMPORT SKRIPTE
+
 use dbBricoMarche;
 
-INSERT INTO Land VALUES('Deutschland');
-INSERT INTO Land VALUES('Österreich');
-INSERT INTO Land VALUES('Schweiz');
+IF NOT OBJECT_ID('temp_plz_at') IS NULL	DROP TABLE temp_plz_at;
+GO
+
+-- HIER WIRD TEMP TABELLE ERSTELLT
+
+-- csv-daten in temporäre tabelle
+BULK
+INSERT temp_plz_at
+FROM 'C:\Users\johanna.lichtenegger\Documents\Visual Studio 2015\Projects\DB_BricoMarcheDACH\GebrauchsDaten\DatenQuellen\plz_at.csv'
+WITH
+(
+FIELDTERMINATOR = ';',
+ROWTERMINATOR = '\n',
+FIRSTROW = 2
+);
+GO
+
+SELECT * FROM temp_plz_at;
+GO
