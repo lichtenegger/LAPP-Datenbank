6d108052a5108e89e14d8fcb576328a669b18767
lichtenegger <johanna.lichtenegger@gmail.com>
2015-11-26 09:32:30 +0100

    DEVDB

--- GebrauchsDaten/Gruppe-Daten.sql
+++
@@ -1,7 +0,0 @@
-use dbBricoMarche;
-
-INSERT INTO Gruppe VALUES('Benutzer');
-INSERT InTO Gruppe VALUES('Administrator');
-GO
-
-use master;
---
+++ TestDaten/B01.bat
@@ -0,0 +1,4 @@
+osql -S 127.0.0.1 -U sa -P 123user! -i B01_TestUser.sql
+
+
+pause
